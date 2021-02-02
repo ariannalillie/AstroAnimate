534c4bdc15187daf0ace0ab20baffdab9194252c
Arianna Johnson <ariannajohnson588@gmail.com>
2021-02-02 13:18:39 -0500

    home page css and button redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "./Componenets/Home"
+import Level1 from "./Componenets/Level1";
 
 function App() {
   return (
+    <>
     <Route exact path="/" >
     <Home />
   </Route>
+      <Route path="/level_1" >
+      <Level1 />
+    </Route>
+    </>
   );
 }
 
---
+++ src/Componenets/Level1/index.js
@@ -0,0 +1,9 @@
+function Level1 () {
+    return (
+        <div>
+            <h1>Welcome to Level 1</h1>
+        </div>
+    )
+}
+
+export default Level1;
